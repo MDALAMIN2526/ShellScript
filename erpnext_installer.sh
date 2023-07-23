1c74d4d4c0604bd70678924390064ab95dc69853
MDALAMIN2526 <amin252646@gmail.com>
2023-07-23 19:50:48 +0600

    first commit

---
+++ erpnext_installer.sh
@@ -0,0 +1,169 @@
+#!/bin/bash
+# STEP 0 Install git
+sudo apt update -y
+sudo apt upgrade -y
+
+# STEP 1 Install git
+sudo apt-get install git -y
+
+# STEP 2 Install python-dev
+sudo apt-get install python3-dev -y
+
+# STEP 3 Install setuptools and pip
+sudo apt-get install python3-setuptools python3-pip -y
+
+# STEP 4 Install virtualenv
+sudo apt-get install virtualenv -y
+
+# CHECK PYTHON VERSION
+python_version=$(python3 -V 2>&1 | grep -oE "[0-9]+\.[0-9]+\.[0-9]+")
+
+# IF VERSION IS 3.8.X RUN
+if [[ "$python_version" == "3.8"* ]]; then
+    sudo apt install python3.8-venv -y
+# IF VERSION IS 3.10.X RUN
+elif [[ "$python_version" == "3.10"* ]]; then
+    sudo apt install python3.10-venv -y
+fi
+
+# STEP 5 Install MariaDB
+sudo apt-get install software-properties-common
+sudo apt install mariadb-server -y
+sudo mysql_secure_installation
+
+# STEP 6 MySQL database development files
+sudo apt-get install libmysqlclient-dev -y
+
+# STEP 7 Edit the mariadb configuration (unicode character encoding)
+# sudo nano /etc/mysql/mariadb.conf.d/50-server.cnf
+# Delete all configuration content to the 50-server.cnf file
+sudo truncate -s 0 /etc/mysql/mariadb.conf.d/50-server.cnf
+# Add the configuration content to the 50-server.cnf file
+
+sudo tee -a /etc/mysql/mariadb.conf.d/50-server.cnf > /dev/null << EOL
+[server]
+user = mysql
+pid-file = /run/mysqld/mysqld.pid
+socket = /run/mysqld/mysqld.sock
+basedir = /usr
+datadir = /var/lib/mysql
+tmpdir = /tmp
+lc-messages-dir = /usr/share/mysql
+bind-address = 127.0.0.1
+query_cache_size = 16M
+log_error = /var/log/mysql/error.log
+
+[mysqld]
+pid-file                = /run/mysqld/mysqld.pid
+basedir                 = /usr
+bind-address            = 127.0.0.1
+expire_logs_days        = 10
+innodb-file-format=barracuda
+innodb-file-per-table=1
+innodb-large-prefix=1
+character-set-client-handshake = FALSE
+character-set-server = utf8mb4
+collation-server = utf8mb4_unicode_ci
+
+[mysql]
+default-character-set = utf8mb4
+EOL
+
+# Now press (Ctrl-X) to exit
+
+sudo service mysql restart
+
+# ... (Remaining steps remain the same)
+
+
+sudo service mysql restart
+
+# STEP 8 Install Redis
+sudo apt-get install redis-server -y
+
+# STEP 9 Install Node.js 14.X package
+sudo apt-get remove nodejs -y
+sudo apt-get remove npm -y
+sudo apt-get update -y
+sudo apt autoremove -y
+which node
+
+sudo apt install curl
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+source ~/.profile
+nvm install 14.15.0   -y
+which node
+
+# STEP 10 Install Yarn
+sudo apt-get install npm -y
+sudo npm install -g yarn -y
+
+# STEP 11 Install wkhtmltopdf
+sudo apt-get install xvfb libfontconfig wkhtmltopdf -y
+
+# STEP 12 Create a new user
+# sudo adduser cpmerp
+# sudo usermod -aG sudo cpmerp
+# su - cpmerp
+chmod -R o+rx /home/$(whoami)
+
+# STEP 13 Install frappe-bench
+sudo -H pip3 install frappe-bench
+bench --version
+
+# STEP 14 Initialize the frappe bench & install frappe latest version
+# Prompt for Frappe Bench version
+echo "Which version of Frappe Bench would you like to install?"
+echo "1. Version 13"
+echo "2. Version 14"
+read -p "Enter the number corresponding to your choice: " bench_version
+
+if [ "$bench_version" = "1" ]; then
+    frappe_branch="version-13"
+    erpnext_branch="version-13"
+elif [ "$bench_version" = "2" ]; then
+    frappe_branch="version-14"
+    erpnext_branch="version-14"
+else
+    echo "Invalid choice. Please enter either 1 or 2."
+    exit 1
+fi
+# STEP 14 Initialize the frappe bench & install frappe latest version
+bench init --frappe-branch "$frappe_branch" frappe-bench
+
+cd frappe-bench/
+nohup bench start &
+
+# STEP 15 Create a site in frappe bench
+cd frappe-bench/
+bench new-site cpm.com
+bench use cpm.com
+
+# STEP 16 Install ERPNext latest version in bench & site
+bench get-app erpnext --branch "$erpnext_branch"
+bench --site cpm.com install-app erpnext
+
+if [ "$bench_version" = "2" ]; then
+    bench get-app payments
+    bench get-app hrms --branch version-14
+    bench --site cpm.com install-app payments
+    bench --site cpm.com install-app hrms
+fi
+bench start
+
+# STEP 17 SETUP PRODUCTION SERVER
+# Enable scheduler service
+bench --site cpm.com enable-scheduler
+
+# Disable maintenance mode
+bench --site cpm.com set-maintenance-mode off
+
+# Setup production config
+sudo bench setup production $(whoami)
+
+# Setup NGINX web server
+bench setup nginx
+
+# Final server setup
+sudo supervisorctl restart all
+sudo bench setup production $(whoami)
